9a41b9a4479254fb05bc76f92385632544236578
MotoMatt5040 <motomatt5040@gmail.com>
2024-06-11 14:04:43 -0500

    added error handler

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import looprunner
+import traceback
 
 if __name__ == '__main__':
-    looprunner.run_loop()
+    try:
+        looprunner.run_loop()
+    except Exception as err:
+        print(traceback.format_exc())
+        input("Press enter to continue")
+
+    input("Press enter to continue")

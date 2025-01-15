e92e8adaf4bb97b0ddb453c9d2b68181ba769698
matt@aero <motomatt5040@gmail.com>
2025-01-15 12:05:28 -0600

    refactor(database-connection): adjust connection info
    Adjusted the connection info for the database class and data puller class to allow hot swapping of databases

--- DataBaseAccessInfo.py
+++ DataBaseAccessInfo.py
@@ -1,23 +1,32 @@
 import os
 
+from sqlalchemy import create_engine
+import webbrowser
+
 
 class DataBaseAccessInfo:
-    CORESERVER = os.environ['coreserver']
-    COREUSER = os.environ["coreuser"]
-    COREPASSWORD = os.environ["corepassword"]
-    DATABASE = os.environ["caligula"]
+    _isInstance = None
 
-    DRIVER = '{ODBC Driver 17 for SQL Server}'
+    def __new__(cls):
+        if cls._isInstance is None:
+            cls._isInstance = super(DataBaseAccessInfo, cls).__new__(cls)
+            cls._initialize(cls._isInstance)
 
-    def __init__(self):
+        return cls._isInstance
+
+    @staticmethod
+    def _initialize(instance):
         try:
-            self._driver = self.DRIVER
-            self._servername = self.CORESERVER
-            self._database = self.DATABASE
-            self._userid = self.COREUSER
-            self._password = self.COREPASSWORD
+            # Initialize the instance if it hasn't been initialized
+            if not hasattr(instance, '_initialized'):
+                instance._driver = '{ODBC Driver 17 for SQL Server}'
+                instance._servername = os.environ['coreserver']
+                instance._database = os.environ['caligula']
+                instance._userid = os.environ['coreuser']
+                instance._password = os.environ['corepassword']
+                instance._initialized = True
         except Exception as err:
-            print(err)
+            print(f"Initialization error: {err}")
 
     def get_info(self):
         URI = '*****\nDriver: {}\nServer Address: {}\nDatabase Name: {}\nUserID: {}\nPassword: {}\n*****'.format(
@@ -33,6 +42,10 @@ class DataBaseAccessInfo:
     def server(self):
         return self._servername
 
+    @server.setter
+    def server(self, server):
+        self._servername = server
+
     @property
     def database(self):
         return self._database
@@ -45,6 +58,49 @@ class DataBaseAccessInfo:
     def user_id(self):
         return self._userid
 
+    @user_id.setter
+    def user_id(self, user_id):
+        self._userid = user_id
+
     @property
     def password(self):
         return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    @property
+    def engine(self):
+        return create_engine(self.dbcon)
+
+    @property
+    def connection(self):
+        return self.engine.connect()
+
+    @property
+    def sql_connection_string(self):
+        return f"DRIVER={self._driver};SERVER={self._servername};DATABASE={self._database};UID={self._userid};PWD={self._password}"
+
+    @property
+    def dbcon(self):
+        return f'mssql+pyodbc:///?odbc_connect={self.sql_connection_string}'
+
+    def connect_engine(self):
+        try:
+            return self.engine.connect()
+        except Exception:
+            print("No ODBC Driver detected. Please install Microsoft ODBC Driver 17 for SQL Server from "
+                  "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
+            webbrowser.open(
+                "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
+            input("Press Enter to continue...")
+
+    def get_config(self):
+        """Method to retrieve the current configuration."""
+        return {
+            'servername': self.server,
+            'database': self.database,
+            'userid': self.user_id,
+            'password': self.password
+        }
--- DataPuller.py
+++ DataPuller.py
@@ -1,8 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
 from sqlalchemy import text
-import webbrowser
-import pyodbc
 from DataBaseAccessInfo import DataBaseAccessInfo
 from SQLDictionary import SQLDictionary
 import traceback
@@ -29,54 +26,20 @@ class DataPuller:
         #  initialize database access info to connect to database
         self.dbai = DataBaseAccessInfo()
 
-        #  Build the connection string
-        self.SQL_CONNECTION = 'DRIVER={};SERVER={};DATABASE={};UID={};PWD={}'.format(
-            self.dbai.driver,
-            self.dbai.server,
-            self.dbai.database,
-            self.dbai.user_id,
-            self.dbai.password
-        )
-
-        self.dbcon = f'mssql+pyodbc:///?odbc_connect={self.SQL_CONNECTION}'
-        # connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": self.SQL_CONNECTION})
-        try:
-            # self.engine = create_engine(self.dbcon, pool_pre_ping=True)
-            self.engine = create_engine(self.dbcon)
-        except Exception as err:
-            error_log(err)
-
-    def __str__(self):
-        return self.SQL_CONNECTION
 
     def active_project_ids(self):
         try:
-            try:
-                cnxn = self.engine.connect()
-            except Exception:
-                print("No ODBC Driver detected. Please install Microsoft ODBC Driver 17 for SQL Server from "
-                      "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
-                webbrowser.open(
-                    "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
+            cnxn = self.dbai.connection
             df = pd.read_sql_query(text(self.sqld.project_id_list()), cnxn)
             cnxn.close()
             del cnxn
             return df
         except Exception as err:
-            raise(err)
-            print(err)
-        return []
+            raise err
 
     def production_report(self, projectid, date) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         try:
-            try:
-                cnxn = self.engine.connect()
-            except Exception:
-                print("No ODBC Driver detected. Please install Microsoft ODBC Driver 17 for SQL Server from "
-                      "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
-                webbrowser.open(
-                    "https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16#version-17")
-                input("Press Enter to continue...")
+            cnxn = self.dbai.connection
             data = self.sqld.production_report(projectid, date)
             productionReport = pd.read_sql_query(text(data[0]), cnxn)
             productionReportDispo = pd.read_sql_query(text(data[1]), cnxn)
@@ -85,6 +48,8 @@ class DataPuller:
             del cnxn
             return productionReport, productionReportDispo, productionReportDailyAVG
         except Exception as err:
-            print(err)
-            input("Press Enter to continue...")
-        return []
+            raise err
+
+
+
+

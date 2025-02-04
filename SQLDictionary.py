9ed0fecb8f0acbcce7415cb5f3e0c63166ad96c5
matt@aero <motomatt5040@gmail.com>
2025-02-04 14:40:16 -0600

    feat(sql): add unscan
    Added unscanned data query, changed some env names

--- SQLDictionary.py
+++ SQLDictionary.py
@@ -16,8 +16,9 @@ class SQLDictionary:
         else:
             time_delta = 1
         projectids = f"{os.environ['active_project_ids']} '{date.today() - timedelta(time_delta)}'"
-        # projectids = "SELECT DISTINCT projectid , recdate FROM tblGPCPHDaily WHERE RecDate >= '2024-07-29' and RecDate <= '2024-07-31' and projectid = '12886C' or projectid = '12886'"
-        # projectids = "SELECT DISTINCT projectid , recdate FROM tblGPCPHDaily WHERE RecDate >= '2025-01-14'"
+        # projectids = "SELECT DISTINCT projectid , recdate FROM tblGPCPHDaily WHERE RecDate >= '2024-07-29' and RecDate <= '2024-07-31' and projectid = '12886C'"#" or projectid = '12886'"
+        # projectids = "SELECT DISTINCT projectid , recdate FROM tblGPCPHDaily WHERE RecDate >= '2025-01-28' and RecDate <= '2025-01-31' and projectid = '13042C' or projectid = '13042'"
+        # projectids = "SELECT DISTINCT projectid , recdate FROM tblGPCPHDaily WHERE RecDate = '2025-01-10'"
         # projectids = f"{os.environ['active project ids']} '2024-05-24'"
         return projectids
 
@@ -34,38 +35,50 @@ class SQLDictionary:
 
         return d
 
-    def voxco_sample_data(self, database: str, start_date: str) -> str:
+    def voxco_sample_data(self, database: str, start_date: datetime) -> str:
         """"""
-        logger.debug(type(start_date))
         end_date = start_date + timedelta(hours=24)
 
         qry = f"""
             SELECT {os.environ['voxco_columns']}
-            FROM [{database}]{os.environ['voxco_table']}
+            FROM [{database}]{os.environ['his_table']}
             WHERE {os.environ['call_date']} >= '{start_date} 10:00' 
             AND {os.environ['call_date']} < '{end_date} 10:00'
             AND {os.environ['voxco_where']}
             """
 
-
         return qry
 
-    def voxco_prel_sample_data(self, database: str, start_date: str) -> str:
+    def voxco_prel_sample_data(self, database: str, start_date: datetime) -> str:
         """"""
-        logger.debug(type(start_date))
         end_date = start_date + timedelta(hours=24)
 
         qry = f"""
             SELECT {os.environ['prel_columns']}
-            FROM [{database}]{os.environ['voxco_table']}
-            INNER JOIN [{database}]{os.environ['prel_table']}
+            FROM [{database}]{os.environ['his_table']}
+            INNER JOIN [{database}]{os.environ['res_table']}
             ON {os.environ['prel_on']}
             WHERE {os.environ['call_date']} >= '{start_date} 10:00' 
             AND {os.environ['call_date']} < '{end_date} 10:00'
             AND {os.environ['rpsq']}
             """
 
-        logger.debug(qry)
-
         return qry
 
+    def voxco_unscanned_sample_data(self, database: str, start_date: datetime) -> str:
+
+        end_date = start_date + timedelta(hours=24)
+
+        qry = f"""SELECT {os.environ['unscanned_columns']}
+            FROM [{database}]{os.environ['his_table']} AS Historic
+            LEFT JOIN [{database}]{os.environ['res_table']} AS Response
+            ON {os.environ['prel_on']}
+            AND Response.{os.environ['rpsq']}
+            WHERE ({os.environ['unscanned_where']})
+            AND {os.environ['a4s']}
+            AND {os.environ['case']}
+            AND {os.environ['call_date']} >= '{start_date} 10:00' 
+            AND {os.environ['call_date']} < '{end_date} 10:00'
+        """
+
+        return qry

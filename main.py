d47e707505cc664d811cad1f2c87af9b6249594b
matt@aero <motomatt5040@gmail.com>
2025-02-05 09:15:03 -0600

    style(main): rem comm
    removed comments

--- main.py
+++ main.py
@@ -9,14 +9,3 @@ if __name__ == '__main__':
         looprunner.run_loop()
     except Exception as err:
         print(traceback.format_exc())
-
-
-# TODO This gives us the COMPLETE dials
-# SELECT COUNT([HisCallNumber])
-#   FROM [Voxco_Project_91722].[dbo].[Historic]
-#   WHERE HisCallDate >= '2024-07-29 11:00:00.000' and HisCallDate <= '2024-07-30 11:00:00.000' and A4SCallNumber IS NULL and HisResult = 'CO'
-
-# TODO Figure out why THIS matches bottom table
-# SELECT COUNT([HisCallNumber])
-#   FROM [Voxco_Project_91722].[dbo].[Historic]
-#   WHERE HisCallDate >= '2024-07-29 11:00:00.000' and HisCallDate <= '2024-07-30 11:00:00.000' and A4SCallNumber IS NULL and HisResult NOT LIKE 'OK'
